3b15d159a3a4e82c25fa69ebead319c78f9ae121
alexandre <alexandrecarvalhodemelo@outlook.com>
2017-09-23 21:52:20 -0300

    config file fix

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '172.26.30.47'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'charis'),
             'username' => env('DB_USERNAME', 'postgres'),
