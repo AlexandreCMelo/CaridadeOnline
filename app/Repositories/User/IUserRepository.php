084a4e705e34a0d9c1d0d6e4b7cd499c75908a2b
alexandre <alexandrecarvalhodemelo@outlook.com>
2017-10-01 15:59:56 -0300

    add user directly to organization

--- app/Repositories/User/IUserRepository.php
+++ app/Repositories/User/IUserRepository.php
@@ -1,5 +1,6 @@
 <?php
-namespace Charis\Repositories\Target;
+
+namespace Charis\Repositories\User;
 
 /**
  * Interface TargetRepository
@@ -8,15 +9,40 @@ namespace Charis\Repositories\Target;
 interface IUserRepository
 {
     public function all();
-    public function add($name, $email, $enabled, $token, $password, $systemRole, $countryId= false, $timezoneId = false, $localeId = false, $origin = false, $phone = false, $organizationId = false, $organizationRole = false);
+
+    public function add(
+        $name,
+        $email,
+        $enabled,
+        $token,
+        $password,
+        $systemRole = false,
+        $phone = false,
+        $countryId = false,
+        $timezoneId = false,
+        $localeId = false,
+        $origin = false,
+        $organizationId = false,
+        $organizationRole = false
+    );
+
     public function remove($id);
+
     public function findUser($id, $email, $countryId, $systemRole, $name, $deleted);
+
     public function findById($id);
+
     public function findByEmail($email);
+
     public function findByCity($cityName);
+
     public function findByCountry($countryId);
+
     public function findBySystemRole($systemRole);
+
     public function findByName($name);
+
     public function findDeletedUsers();
+
     public function addOrganizationRole($userId, $organizationId, $roleId);
 }
