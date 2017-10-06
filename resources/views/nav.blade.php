cf8e0cba17ca4fb3814f9613ea9b0d6312491d6c
Danilo Silva <danilocgsilva@gmail.com>
2017-10-06 10:04:53 -0300

    First menu changes

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,23 +1,51 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-    <div class="container">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
+    <a class="navbar-brand" href="#"></a>
 
-                @foreach ($menuOptions as $option)
-                    <li><a href="/">{{ $option }}</a></li>
-                @endforeach
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        </div><!--/.nav-collapse -->
-    </div><!--/.container-fluid -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Usuários</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Map</a>
+            </li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+            @guest
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown link
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link" href="/register">Cadastro</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/login">Login</a>
+            </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">{{ Auth::user()->name }}</a>
+                    </li>
+            @endguest
+        </ul>
+
     </div>
 </nav>
