bd2c0fe1017983858f33ee53fb194050c5f0da26
Danilo Silva <danilocgsilva@gmail.com>
2017-11-17 22:39:04 -0200

    SocialLogin classes has been simplified down to the extreme to works with just a solitary constant

--- app/SocialLogin/SocialLoginFacebook.php
+++ app/SocialLogin/SocialLoginFacebook.php
@@ -2,44 +2,7 @@
 
 namespace Charis\SocialLogin;
 
-use Illuminate\Contracts\Auth\Guard as Authenticator;
-use Charis\Models\User;
-
-class SocialLoginFacebook implements ISocialLogin
+class SocialLoginFacebook extends ASocialLogin
 {
-
     const PROVIDER = 'facebook';
-
-
-    public $auth;
-
-
-    public function __construct(Authenticator $auth)
-    {
-        $this->auth = $auth;
-    }
-
-
-    public function getPermission()
-    {
-        return \Socialite::with($this::PROVIDER)->redirect();
-    }
-
-
-    public function callback()
-    {
-        $userData = \Socialite::with($this::PROVIDER)->user();
-        $user = $this->saveOrRecover($userData);
-        $this->auth->login($user, true);
-        return redirect(route('home'));
-    }
-
-
-    public function saveOrRecover($userData)
-    {
-        return User::firstOrCreate(
-            ['name' => "$userData->name", 'email' => "$userData->email"]
-        );
-    }
-
 }
--- app/SocialLogin/SocialLoginLinkedin.php
+++ app/SocialLogin/SocialLoginLinkedin.php
@@ -2,41 +2,7 @@
 
 namespace Charis\SocialLogin;
 
-use Illuminate\Contracts\Auth\Guard as Authenticator;
-use Charis\Models\User;
-
 class SocialLoginLinkedin implements ISocialLogin
 {
     const PROVIDER = 'linkedin';
-
-    public $auth;
-
-
-    public function __construct(Authenticator $auth)
-    {
-        $this->auth = $auth;
-    }
-
-
-    public function getPermission()
-    {
-        return \Socialite::with($this::PROVIDER)->redirect();
-    }
-
-
-    public function callback()
-    {
-        $userData = \Socialite::with($this::PROVIDER)->user();
-        $user = $this->saveOrRecover($userData);
-        $this->auth->login($user, true);
-        return redirect(route('home'));
-    }
-
-
-    public function saveOrRecover($userData)
-    {
-        return User::firstOrCreate(
-            ['name' => "$userData->name", 'email' => "$userData->email"]
-        );
-    }
 }
