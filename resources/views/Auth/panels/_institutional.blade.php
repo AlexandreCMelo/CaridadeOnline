d2bc53cb34fd2069da6cbe62191d2a4d437bf428
Danilo Silva <danilocgsilva@gmail.com>
2017-11-30 13:40:46 -0200

    Screen 2 login styles completed, but needs revision facing the ui kit

--- resources/views/Auth/panels/_institutional.blade.php
+++ resources/views/Auth/panels/_institutional.blade.php
@@ -1,27 +1,27 @@
 <div class="charis-panel">
-  
+
   <h2>É uma empresa?</h2>
 
   <p>Caso tenha uma empresa e queira fazer parte do Caridade Online, é necessário ter em mãos CNPJ e ser responsável por ela. Preencha abaixo o formulário para iniciar.</p>
 
-  <form class="" action="index.html" method="post">
+  <form class="extra-padding text-align-right" action="index.html" method="post">
 
     <div class="form-group">
-      <input type="text" class="form-control" id="" placeholder="Email principal">
+      <input type="text" class="form-control form-control-charis" id="" placeholder="Email principal">
     </div>
 
     <div class="form-group">
-      <input type="text" class="form-control" id="" placeholder="Nome Fantasia">
+      <input type="text" class="form-control form-control-charis" id="" placeholder="Nome Fantasia">
     </div>
 
     <div class="form-group">
-      <input type="text" class="form-control" id="" placeholder="CNPJ">
+      <input type="text" class="form-control form-control-charis" id="" placeholder="CNPJ">
     </div>
 
-    <p>Esqueci minha senha</p>
+    <small><a href="#" class="align-right">Esqueci minha senha</a></small>
 
     <div class="form-group">
-      <input type="submit" class="form-control" id="" value="Cadastrar">
+      <input type="submit" class="btn btn-charis-color" id="" value="Cadastrar">
     </div>
 
   </form>
--- resources/views/Auth/panels/_personal.blade.php
+++ resources/views/Auth/panels/_personal.blade.php
@@ -5,32 +5,61 @@
 
   <p>Após criar uma conta, você precisa confirmar ela no email que recebeu.</p>
 
-  <div class="row">
-    <div class="col-4">
-      <a href="#" class="btn btn-default btn-block">Login com Facebook</a>
-      <a href="#" class="btn btn-default btn-block">Login com Linkedin</a>
-      <a href="#" class="btn btn-default btn-block">Login com Google</a>
+  <div class="row extra-padding">
+
+    <div class="col-xl-4">
+
+      <a href="#" class="btn
+                        btn-default
+                        btn-block
+                        btn-charis
+                        btn-charis-left
+                        btn-charis-facebook">
+        <i class="fa fa-facebook" aria-hidden="true"></i>
+        Login com Facebook
+      </a>
+
+      <a href="#" class="btn
+                         btn-default
+                         btn-block
+                         btn-charis
+                         btn-charis-left
+                         btn-charis-linkedin">
+        <i class="fa fa-linkedin" aria-hidden="true"></i>
+        Login com Linkedin
+      </a>
+
+      <a href="#" class="btn
+                         btn-default
+                         btn-block
+                         btn-charis
+                         btn-charis-left
+                         btn-charis-google">
+        <i class="fa fa-google" aria-hidden="true"></i>
+        Login com Google
+      </a>
+
     </div>
 
-    <div class="col-8">
+    <div class="col-xl-8 text-align-right mobile-separator">
       <form class="" action="index.html" method="post">
 
         <div class="form-group">
-          <input type="text" class="form-control" id="" placeholder="Email">
+          <input type="text" class="form-control form-control-charis" id="" placeholder="Email">
         </div>
 
         <div class="form-group">
-          <input type="text" class="form-control" id="" placeholder="Senha">
+          <input type="text" class="form-control form-control-charis" id="" placeholder="Senha">
         </div>
 
-        <p>Esqueci minha senha</p>
+        <small><a href="#" class="right">Esqueci minha senha</a></small>
 
         <div class="form-group">
-          <input type="submit" class="form-control" id="" value="Acessar">
+          <input type="submit" class="btn btn-charis-color" id="" value="Acessar">
         </div>
 
       </form>
     </div>
   </div>
-  
+
 </div>
